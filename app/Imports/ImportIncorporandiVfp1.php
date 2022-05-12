18a03b02f23d16cfb82fce6578ec71d3b8123553
Tr3Spark <s_park_91@hotmail.it>
2022-05-12 21:02:23 +0200

    Fixata la prima riga della tabella excell dopo l'import

--- app/Imports/ImportIncorporandiVfp1.php
+++ app/Imports/ImportIncorporandiVfp1.php
@@ -4,9 +4,11 @@ namespace App\Imports;
 use Carbon\Carbon;
 use App\Models\IncorporandiVfp1;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Imports\HeadingRowFormatter;
 use Maatwebsite\Excel\Row;
-
-class ImportIncorporandiVfp1 implements ToModel
+HeadingRowFormatter::default('none');
+class ImportIncorporandiVfp1 implements ToModel, WithHeadingRow
 {
     /**
     * @param array $row
@@ -17,44 +19,44 @@ class ImportIncorporandiVfp1 implements ToModel
     public function model(array $row)
     {        
         return new IncorporandiVfp1([
-            'ID' => $row[0],	
-            'ANNO'  => $row[1],	
-            'BLOCCO'  => $row[2],	
-            'INC'  => $row[3],	
-            'COGNOME'  => $row[4],	
-            'NOME'  => $row[5],	
-            'MATRICOLA'  => $row[6],	
-            'EX-MATRICOLA'  => $row[7],	
-            'CORPO'  => $row[8],	
-            'CATEGORIA'  => $row[9],	
-            'ABILITAZIONE'  => $row[10],	
-            'SPECIALITA'  => $row[11],
-            'PROFILO DI IMPIEGO'  => $row[12],
-            'SESSO'  => $row[13],
-            'COMP'  => $row[14],
-            'DATA INCORPORAMENTO' =>Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject((int)$row[15])),
-            'DATA AMMINISTRATIVA' => $row[16],	
-            'CODICE FISCALE'  => $row[17],
-            'DATA DI NASCITA'  => $row[18],	
-            'COMUNE DI NASCITA'  => $row[19],	
-            'PROVINCIA DI NASCITA'  => $row[20],	
-            'CAP DI NASCITA'  => $row[21],	
-            'REGIONE APP'  => $row[22],	
-            'COMUNE RESIDENZA'  => $row[23],	
-            'PROVINCIA RESIDENZA'  => $row[24],
-            'INDIRIZZO RESIDENZA'  => $row[25],	
-            'CIVICO RESIDENZA'  => $row[26],
-            'CAP RESIDENZA'  => $row[27],	
-            'COMUNE DOMICILIO'  => $row[28],	
-            'PROVINCIA DOMICILIO'  => $row[29],	
-            'INDIRIZZO DOMICILIO'  => $row[30],	
-            'NUMERO CIVICO DOMICLIO'  => $row[31], 	
-            'CAP DOMICILIO'  => $row[32],	
-            'CELLULARE FAMILIARE'  => $row[33],	
-            'CELLULARE PERSONALE'  => $row[34],	
-            'E-MAIL'  => $row[35],
-            'PEC'  => $row[36],	
-            'INCORPORATO'=> $row[37],	
+            'ID' => $row['ID'],	
+            'ANNO'  => $row['ANNO'],	
+            'BLOCCO'  => $row['BLOCCO'],	
+            'INC'  => $row['INC'],	
+            'COGNOME'  => $row['COGNOME'],	
+            'NOME'  => $row['NOME'],	
+            'MATRICOLA'  => $row['MATRICOLA'],	
+            'EX-MATRICOLA'  => $row['EX-MATRICOLA'],	
+            'CORPO'  => $row['CORPO'],	
+            'CATEGORIA'  => $row['CATEGORIA'],	
+            'ABILITAZIONE'  => $row['ABILITAZIONE'],	
+            'SPECIALITA'  => $row['SPECIALITA'],
+            'PROFILO DI IMPIEGO'  => $row['PROFILO DI IMPIEGO'],
+            'SESSO'  => $row['SESSO'],
+            'COMP'  => $row['COMP'],
+            'DATA INCORPORAMENTO' =>Carbon::instance(\PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject((int)$row['DATA INCORPORAMENTO'])),
+            'DATA AMMINISTRATIVA' => $row['DATA AMMINISTRATIVA'],	
+            'CODICE FISCALE'  => $row['CODICE FISCALE'],
+            'DATA DI NASCITA'  => $row['DATA DI NASCITA'],	
+            'COMUNE DI NASCITA'  => $row['COMUNE DI NASCITA'],	
+            'PROVINCIA DI NASCITA'  => $row['PROVINCIA DI NASCITA'],	
+            'CAP DI NASCITA'  => $row['CAP DI NASCITA'],	
+            'REGIONE APP'  => $row['REGIONE APP'],	
+            'COMUNE RESIDENZA'  => $row['COMUNE RESIDENZA'],	
+            'PROVINCIA RESIDENZA'  => $row['PROVINCIA RESIDENZA'],
+            'INDIRIZZO RESIDENZA'  => $row['INDIRIZZO RESIDENZA'],	
+            'CIVICO RESIDENZA'  => $row['CIVICO RESIDENZA'],
+            'CAP RESIDENZA'  => $row['CAP RESIDENZA'],	
+            'COMUNE DOMICILIO'  => $row['COMUNE DOMICILIO'],	
+            'PROVINCIA DOMICILIO'  => $row['PROVINCIA DOMICILIO'],	
+            'INDIRIZZO DOMICILIO'  => $row['INDIRIZZO DOMICILIO'],	
+            'NUMERO CIVICO DOMICLIO'  => $row['NUMERO CIVICO DOMICILIO'], 	
+            'CAP DOMICILIO'  => $row['CAP DOMICILIO'],	
+            'CELLULARE FAMILIARE'  => $row['CELLULARE FAMILIARE'],	
+            'CELLULARE PERSONALE'  => $row['CELLULARE PERSONALE'],	
+            'E-MAIL'  => $row['E-MAIL'],
+            'PEC'  => $row['PEC'],	
+            'INCORPORATO'=> $row['INCORPORATO'],	
         ]);
     }
 }
