c3c03c55e54631b03d8a347ed242f4a03f064aca
antocala98 <anto98372@gmail.com>
2022-05-17 11:15:30 +0200

    routes aggiornate

--- resources/views/corsi/admin/gestionepersonalecorsi.blade.php
+++ resources/views/corsi/admin/gestionepersonalecorsi.blade.php
@@ -34,11 +34,15 @@
                                 {{$user->tipo_utente}}
                             </td>
                             <td class="px-6 py-4 text-right">
-                                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                <form method="POST" action="{{url('corsi/22-nmrs/admin/gestione-personale-corsi/$user->id')}}">
+                                    @csrf
+                                    <button type="submit" class="font-medium text-blue-600 dark:text-blue-500 hover:underline" >Edit</button>
+                                </form>
+                                
                             </td>
                         </tr>
                         @endforeach
-                    </tbody>
+                    </tbody>      
                 </table>
             </div>
         </div>
