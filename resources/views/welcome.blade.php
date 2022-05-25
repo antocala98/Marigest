28a22371e79e6d761ac17d2de589441bfec683f7
antocala98 <anto98372@gmail.com>
2022-05-25 15:35:06 +0200

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,7 +28,7 @@
                 <div class="flex justify-center items-center h-full">
                     <div class="text-center text-white px-6 md:px-12">
                         <h1 class="text-5xl font-bold mt-0 mb-6" role="region" aria-label="MARIGEST">MARIGEST</h1>
-                        <h3>La piattaforma per la gestione delle scuole della Marina Militare Italiana</h3>
+                        <h3>La piattaforma per la gestione delle Scuole della Marina Militare Italiana</h3>
                 <!-- <button type="button"
                             class="inline-block px-6 py-2.5 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
                             data-mdb-ripple="true" data-mdb-ripple-color="light">
