8fd89e5c49de96ec0cca1b54139ab7cafe54db41
Giorgio Fonte <s_park_91@hotmail.it>
2022-05-28 00:46:07 +0200

    form per l'inserimento dei verbali

--- resources/views/corsi/admin/sezioneStudi/aggiungiVerbaleEsame.blade.php
+++ resources/views/corsi/admin/sezioneStudi/aggiungiVerbaleEsame.blade.php
@@ -14,7 +14,7 @@
     </li>
 @endsection
 @section('body')
-<?php if(isset($feedback_utente)){ ?>
+<!--<?php if(isset($feedback_utente)){ ?>
     <div id="toast-success"
         class="flex items-center w-full p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
         role="alert">
@@ -37,13 +37,13 @@
                     clip-rule="evenodd"></path>
             </svg>
         </button>
-<?php } else { ?>
+<?php } else { ?>-->
     <form method="POST" action="#" id="form" enctype="multipart/form-data">
         @csrf
-        <div class="m-11 grid gap-6 mb-24 lg:grid-cols-2">
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                <x-auth-validation-errors class="ml-20" :errors="$errors" />
-                <div class="mt-4">
+        <div class="flex justify-center ">
+            <div class="w-full mx-8 sm:max-w-md mt-12 px-2 py-2 bg-white shadow-md overflow-hidden sm:rounded-lg">
+                <x-auth-validation-errors class="ml-2" :errors="$errors" />
+                <!--<div class="mt-4">
                     <label for="tipo_provvedimento" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Tipo provvedimento</label>
                     <select id="tipo_provvedimento" name="tipo_provvedimento" class="block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                         <option value="Rimprovero">Rimprovero</option>
@@ -52,46 +52,55 @@
                         <option value="Elogio">Elogio</option>
                         <option value="TPS">T.P.S.</option>
                     </select>
+                </div>-->
+                <div class="mt-4">
+                    <label for="cod_verb" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Codice Verbale</label>
+                    <input type="number" id="codiceVerbale" name="codiceVerbale" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >
                 </div>
                 <div class="mt-4">
-                    <label for="num_giorni" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Numero giorni provvedimento</label>
-                    <input disabled type="number" id="num_giorni" name="num_giorni" min="1" max="15" step="1" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >
+                    <label for="materie" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Materie</label>
+                    <input list="materie" type="text" name="materie" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 </div>
                 <div class="mt-4">
-                    <label for="data_provvedimento" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Data provvedimento</label>
-                    <input type="date" id="data_provvedimento" name="data_provvedimento" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                </div>
-                <div class="mt-4">
-                    <label for="data_notifica" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Data di notifica</label>
-                    <input type="date" id="data_notifica" name="data_notifica" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <label for="data_verbale" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Data Verbale</label>
+                    <input type="date" id="dataVerbale" name="dataVerbale" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 </div>
             </div>
     
-            <div class="w-full pl-6 sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-full p-2 mx-8 sm:max-w-md mt-12 px-6 py-2 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 <div class="mt-4">
                     <label for="allievo" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Allievo</label>
                     <input list="allievi" type="text" id="allievo" name="allievo" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 </div>
                 <div class="mt-4">
-                    <label for="n_protocollo" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Numero di protocollo</label>
-                    <input type="text" id="n_protocollo" name="n_protocollo" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <label for="voto" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Voto</label>
+                    <input type="text" id="voto" name="voto" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 </div>
-                <label for="note_aggiutive" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Note aggiuntive</label>
-                <textarea name="note_aggiuntive" id="note_aggiuntive" form="form"></textarea>
+                <div class="mt-4">
+                    <label for="ufficiale" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Ufficiale di commissione</label>
+                    <input list="ufficiale" type="text" id="ufficiale" name="ufficiale" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                </div>
+                <div class="hidden">
+                    <input type="id" name="idUserRedattore" value="{{$userRedattore->id}}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                </div>
             </div>
         </div>
-            <div class="flex justify-center">
+            <div class="flex justify-center mt-6">
                 <x-button element="button" class="mb-24" type="submit">Inserisci</x-button>
-    
             </div>
     
     </form>
+    <datalist id="materie">
+        @foreach ($materie as $materia)
+            <option value="{{$materia->codice}}">{{$materia->nome}}-{{$materia->facolta}}-{{$materia->codice}}</option>
+        @endforeach
+    </datalist>
     <datalist id="allievi">
         @foreach ($allievi as $allievo)
             <option value="{{$allievo->matricola_militare}}">{{$allievo->cognome}} {{$allievo->nome}}-{{$allievo->matricola_militare}}</option>
         @endforeach
     </datalist>
-    <?php } ?>
+    <!--<?php } ?>
     <script>
         document.getElementById('tipo_provvedimento').onchange = function () {
             document.getElementById('num_giorni').setAttribute("disabled", "disabled");
@@ -99,5 +108,5 @@
                 document.getElementById('num_giorni').removeAttribute("disabled");
             }
         };
-    </script>
+    </script>-->
 @endsection
