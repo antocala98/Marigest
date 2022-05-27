7ac457180433b72a998b9bfe0ef2935531c73091
MalumaGianni <102587588+MalumaGianni@users.noreply.github.com>
2022-05-27 13:20:07 +0200

    Aggiornamento Allegato "D" e visualizza provv. disciplinare

--- resources/views/corsi/admin/visualizzaProvDisciplinare.blade.php
+++ resources/views/corsi/admin/visualizzaProvDisciplinare.blade.php
@@ -21,5 +21,54 @@
     </li>
 @endsection
 @section('body')
-  
+
+    <div class="p-4 pb-14 bg-gray-50 rounded-lg dark:bg-gray-800 " id="profile" role="tabpanel"
+         aria-labelledby="profile-tab">
+        <div id="accordion-collapse" data-accordion="collapse" class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table class="w-full table-fixed pb-12 overflow-auto text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Matricola
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Provvedimento
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Giorni
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Data Provvedimento
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Data Notifica
+                    </th>
+                </tr>
+                </thead>
+                <tbody class="">
+
+                @foreach ($provvedimentoDisciplinare as $provvedimentoD)
+                    <tr id="accordion-collapse" data-accordion="collapse" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                            {{$provvedimentoD->matricola_allievo}}
+                        </th>
+                        <td class="px-6 py-4">
+                            {{$provvedimentoD->tipo_provvedimento}}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{$provvedimentoD->num_giorni_provvedimento}}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{$provvedimentoD->data_provvedimento}}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{$provvedimentoD->data_notifica}}
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+    </div>
 @endsection
