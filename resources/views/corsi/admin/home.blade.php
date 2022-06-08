c5939da2101a3bf567ed9331a09e55e075d46d97
Giorgio Fonte <s_park_91@hotmail.it>
2022-06-08 17:47:19 +0200

    finale finale finale

--- resources/views/corsi/admin/home.blade.php
+++ resources/views/corsi/admin/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.layoutAdminCorsi')
 @section('body')
-<div class="pl-10 pt-10 pb-0 grid grid-cols-1 gap-4 md:grid md:grid-cols-2 lg:grid lg:grid-cols-3">
+<div class="px-auto sm:pl-8 pt-10 pb-0 grid grid-cols-1 gap-4 md:grid md:grid-cols-2 lg:grid lg:grid-cols-3">
     <div div class=" mb-6 p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 ">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Sezione Disciplinare</h5>
     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Gestione della situazione disciplinare degli allievi</p>
